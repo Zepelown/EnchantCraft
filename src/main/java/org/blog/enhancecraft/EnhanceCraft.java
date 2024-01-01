1edb0463629b5516a14e3be63cfb67f6421fc864
SeongWonYoon <dbswjdqhd12@gmail.com>
2024-01-01 23:20:33 +0900

    [Refactor] 변경 프로젝트 이름

--- src/main/java/org/blog/enhancecraft/EnhanceCraft.java
+++ src/main/java/org/blog/enhancecraft/EnhanceCraft.java
@@ -1,23 +1,19 @@
-package org.blog.enchantcraft;
+package org.blog.enhancecraft;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class EnchantCraft extends JavaPlugin {
-    public static EnchantCraft serverInstance;
+public final class EnhanceCraft extends JavaPlugin {
+
     @Override
     public void onEnable() {
-        serverInstance = this;
         // Plugin startup logic
         getLogger().info("플러그인 시작 테스트");
+        //
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         getLogger().info("플러그인 종료 테스트");
-    }
-
-    public static EnchantCraft getServerInstance() {
-        return serverInstance;
     }
 }
